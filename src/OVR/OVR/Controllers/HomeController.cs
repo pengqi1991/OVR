f48be2183ee300a8778c7f86f2346e71a14cff40
PQ <pengqi1991@126.com>
2020-08-10 17:37:11 +0800

    增加layer bootstrap4.5

--- src/OVR/OVR/Controllers/HomeController.cs
+++ src/OVR/OVR/Controllers/HomeController.cs
@@ -5,7 +5,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OVR.Filters;
 using OVR.Models;
+using OVR.WebCore.Core;
 
 namespace OVR.Controllers
 {
@@ -17,7 +19,7 @@ namespace OVR.Controllers
         {
             _logger = logger;
         }
-
+        [AuthorizeFilter]
         public IActionResult Index()
         {
             _logger.LogError("aaaaa");
@@ -25,11 +27,66 @@ namespace OVR.Controllers
             
             return View();
         }
-
+        public IActionResult Login()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
         }
+
+
+        #region 实现登录
+        [HttpPost]
+        public async Task<IActionResult> LoginJson(string userName, string password)
+        {
+            TData obj = new TData();
+            
+
+
+
+
+            TData<UserEntity> userObj = await userBLL.CheckLogin(userName, password, (int)PlatformEnum.Web);
+            if (userObj.Tag == 1)
+            {
+                await new UserBLL().UpdateUser(userObj.Data);
+                await Operator.Instance.AddCurrent(userObj.Data.WebToken);
+            }
+
+            string ip = NetHelper.Ip;
+            string browser = NetHelper.Browser;
+            string os = NetHelper.GetOSVersion();
+            string userAgent = NetHelper.UserAgent;
+
+            Action taskAction = async () =>
+            {
+                LogLoginEntity logLoginEntity = new LogLoginEntity
+                {
+                    LogStatus = userObj.Tag == 1 ? OperateStatusEnum.Success.ParseToInt() : OperateStatusEnum.Fail.ParseToInt(),
+                    Remark = userObj.Message,
+                    IpAddress = ip,
+                    IpLocation = IpLocationHelper.GetIpLocation(ip),
+                    Browser = browser,
+                    OS = os,
+                    ExtraRemark = userAgent,
+                    BaseCreatorId = userObj.Data?.Id
+                };
+
+                // 让底层不用获取HttpContext
+                logLoginEntity.BaseCreatorId = logLoginEntity.BaseCreatorId ?? 0;
+
+                await logLoginBLL.SaveForm(logLoginEntity);
+            };
+            AsyncTaskHelper.StartTask(taskAction);
+
+            obj.Tag = userObj.Tag;
+            obj.Message = userObj.Message;
+            return Json(obj);
+        }
+        #endregion
+
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ src/OVR/OVR/Filters/AuthorizeFilterAttribute.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using OVR.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OVR.Filters
+{
+    public class AuthorizeFilterAttribute : ActionFilterAttribute
+    {
+        public AuthorizeFilterAttribute() { }
+
+        
+
+        /// <summary>
+        /// 权限字符串，例如 organization:user:view
+        /// </summary>
+        public string Authorize { get; set; }
+
+
+
+        public const string TokenName = "UserToken";
+
+        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            
+            string token = string.Empty;
+            token = context.HttpContext.Request.Cookies[TokenName];
+
+
+            if (string.IsNullOrEmpty(token))
+            {
+                context.Result = new RedirectResult("~/Home/Login");
+                return;
+            }
+            else
+            {
+                 await next();
+            }
+
+
+            
+           
+        }
+    }
+}
